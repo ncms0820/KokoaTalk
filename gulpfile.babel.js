8a4ae48cac3372a6e02f0c55e0dbc5de99d1bab3
Minsu Cho <ncms0820@outlook.com>
2022-03-29 01:54:26 +0900

    test

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,58 +1,56 @@
 import gulp from "gulp";
+import gpug from "gulp-pug";
 import del from "del";
+import ws from "gulp-webserver";
+import image from "gulp-image";
 import gulpSass from "gulp-sass";
 import dartSass from "sass";
-import minify from "gulp-csso";
-import autoprefixer from "gulp-autoprefixer";
-import image from "gulp-image";
+import autop from "gulp-autoprefixer";
+import miniCSS from "gulp-csso";
 import bro from "gulp-bro";
 import babelify from "babelify";
-import ws from "gulp-webserver";
 import ghPages from "gulp-gh-pages";
 
 const sass = gulpSass(dartSass);
 
 const routes = {
-  css: {
-    watch: "src/scss/*",
-    src: "src/scss/styles.scss",
-    dest: "build/css",
+  pug: {
+    watch: "src/**/*.pug",
+    src: "src/*.pug",
+    dest: "build",
   },
   img: {
     src: "src/img/*",
     dest: "build/img",
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css",
   },
   js: {
     watch: "src/js/**/*.js",
     src: "src/js/main.js",
     dest: "build/js",
   },
-  html: {
-    src: "src/*.html",
-    dest: "build/",
-  },
 };
+
+const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+
+const clean = () => del(["build"]);
 
 const webserver = () => gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
-const html = async () => {
-  gulp.src(routes.html.src).pipe(gulp.dest(routes.html.dest));
-};
+const img = () => gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 
 const styles = () =>
   gulp
-    .src(routes.css.src)
+    .src(routes.scss.src)
     .pipe(sass.sync().on("error", sass.logError))
-    .pipe(
-      autoprefixer({
-        flexbox: true,
-        grid: "autoplace",
-      })
-    )
-    .pipe(minify())
-    .pipe(gulp.dest(routes.css.dest));
+    .pipe(autop())
+    .pipe(miniCSS())
+    .pipe(gulp.dest(routes.scss.dest));
 
-const img = () => gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 const js = () =>
   gulp
     .src(routes.js.src)
@@ -66,20 +64,18 @@ const js = () =>
 const deployer = () => gulp.src("build/**/*").pipe(ghPages());
 
 const watch = () => {
-  gulp.watch(routes.css.watch, styles);
+  gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, styles);
   gulp.watch(routes.js.watch, js);
-  gulp.watch(routes.html.src, html);
 };
-
-const clean = () => del(["build/"]);
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([styles, js, html]);
+const assets = gulp.series([pug, styles, js]);
 
-const live = gulp.parallel([watch, webserver]);
+const live = gulp.parallel([webserver, watch]);
 
 export const build = gulp.series([prepare, assets]);
 export const dev = gulp.series([build, live]);
-export const deploy = gulp.series([build, deployer, clean]);
+export const deploy = gulp.series([build, deployer]);
